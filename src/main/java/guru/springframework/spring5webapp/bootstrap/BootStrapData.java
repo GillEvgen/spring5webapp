fc0c31ddb5801c2d1d7d370255cbcf97953e2f29
GillEvgen <ggg1786@gmail.com>
2020-05-02 13:55:49 +0300

    change bootstrap

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repository.AuthorRepository;
 import guru.springframework.spring5webapp.repository.BookRepository;
+import guru.springframework.spring5webapp.repository.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,24 +14,46 @@ public class BootStrapData implements CommandLineRunner {
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        System.out.println("Started bootstrap");
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Dark Dolin", "123456");
+        Publisher dave = new Publisher("Dave", "Brest, BEL, 1234");
+        Publisher evgenii = new Publisher("Evgenii", "Moscow, RUS, 123445");
+
 
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(dave);
+        publisherRepository.save(evgenii);
 
-        System.out.println("Started bootstrap");
+
+        Author evgen = new Author("Gill", "Evgen");
+        Book bookEvgen = new Book("Gore ot uma", "12345678");
+
+        evgen.getBooks().add(bookEvgen);
+        bookEvgen.getAuthors().add(evgen);
+
+        authorRepository.save(evgen);
+        bookRepository.save(bookEvgen);
+
+        System.out.println("Publisher count " + publisherRepository.count());
         System.out.println("Book count " + bookRepository.count());
     }
 }
